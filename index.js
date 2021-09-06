0eb694beb4ed4cbed2c1f77edcbf702026ccb763
Ryan Blomquist <snakey699@yahoo.com>
2021-09-05 22:16:06 -0500

    Implemented basic game rules.

--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 //Make the deck appear centered and face down.
+cards.init({table:'#card-table', type:STANDARD});
 let deck;
 let playerhand;
-let dealerhand;
+let faceUpDealerHand;
+const DEALER_DECK_Y = 60;
+const DEALER_DECK_X = 255;
 startGame();
 
 function startGame() {
-    cards.init({table:'#card-table', type:STANDARD});
     deck = new cards.Deck();
+    cards.shuffle(deck);
     deck.addCards(cards.all);
     deck.render({immediate:true});
-    dealerhand = new cards.Hand({faceUp:false, y:60});
+    faceUpDealerHand = new cards.Hand({
+        faceUp:true
+        , y: DEALER_DECK_Y
+        , x: DEALER_DECK_X
+    });
+    faceDownDealerHand = new cards.Hand({faceUp: false
+        , y: DEALER_DECK_Y
+        , x: DEALER_DECK_X + 80
+    });
     playerhand = new cards.Hand({faceUp:true, y:340});
 
-    deck.deal(2, [dealerhand, playerhand], 50, () => {
-        console.log("dealer hand");
-        calculateScore(dealerhand);
-        console.log("player hand");
-        calculateScore(playerhand);
+    deck.deal(2, [playerhand], 50, () => {
+        decideResult(playerhand);
     });
 
+    deck.deal(1, [faceUpDealerHand, faceDownDealerHand], 50);
+
     deck.click(card => {
-        if (card === deck.topCard()){
-            drawCard(playerhand);
-        }
+        playerhand.addCard(card);
+        playerhand.render();
+        decideResult(playerhand);
     });
 }
 
@@ -31,16 +41,49 @@ $('#reset').click(() => {
     startGame();
 });
 
-function drawCard(hand) {
-    hand.addCard(deck.topCard());
-    hand.render();
-    calculateResult(hand);
+$('#done').click(() => {
+    faceDownDealerHand.faceUp = true;
+    faceDownDealerHand.render();
+    determineGameOutcome();
+});
+
+function determineGameOutcome() {
+    let dealerScore = calculateDealerScore();
+
 }
 
-function calculateResult(hand) {
+function calculateDealerScore() {
+    tempHand = new cards.Hand();
+    let i = 0;
+    tempHand[i] = faceUpDealerHand[i];
+    i++;
+    for (let j = 0; j < faceDownDealerHand.length; j++) {
+        tempHand[i] = faceDownDealerHand[j];
+        i++;
+    }
+    tempHand["length"] = i;
+    let score = calculateScore(tempHand);
+    let k = 1;
+    while (score < 17) {
+        score += deck.topCard().rank;
+        setTimeout(function() {
+            faceDownDealerHand.addCard(deck.topCard());
+            faceDownDealerHand.render();
+            $('#dealer-score-display').text(score);
+        }, 1000 * k);
+        k++;
+    }
+    return score;
+}
+
+function decideResult(hand) {
     let score = calculateScore(hand);
-    if (score > 21 && hand == playerhand) {
-        gameOver();
+    console.log(score);
+    if (hand === playerhand) {
+        $('#player-score-display').text(score);
+        if (score > 21) {
+            gameOver();
+        }
     }
 }
 
@@ -48,15 +91,14 @@ function calculateScore(hand) {
     let score = 0;
     let aceCount = 0
     for (let i = 0; i < hand.length; i++) {
-        setScore(i);
+        scoreCards(i);
     }
     if (score > 21 && aceCount > 0) {
-        setAceValues();
+        rescoreAces();
     }
-    $('#player-score-display').text(score);
     return score;
 
-    function setScore(i) {
+    function scoreCards(i) {
         let cardValue = hand[i].rank;
         if (cardValue > 10) {
             score += 10;
@@ -70,7 +112,7 @@ function calculateScore(hand) {
         }
     }
 
-    function setAceValues() {
+    function rescoreAces() {
         let i = 0;
         while (score > 21 && i < aceCount) {
             //aces were originally treated as 11, so subtracting 10 treats them as 1
@@ -82,5 +124,8 @@ function calculateScore(hand) {
 
 function gameOver() {
     $('#game-over').show();
+    faceDownDealerHand.faceUp = true;
+    faceDownDealerHand.render();
     deck.click(() => {});
+    $('#dealer-score-display').text(calculateDealerScore(faceUpDealerHand));
 }
